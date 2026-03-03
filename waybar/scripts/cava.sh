4479e6a65496d0f094a5b321bf47b498f9b578f5
zamanlof <zamanrejaz@gmail.com>
2026-03-03 11:39:40 +0530

    some changes

--- waybar/scripts/cava.sh
+++ waybar/scripts/cava.sh
@@ -19,10 +19,27 @@ while IFS= read -r raw; do
   for n in $cleaned; do
     # Make sure n is a number before doing math
     if [[ "$n" =~ ^[0-9]+$ ]]; then
-      level=$(( n / 250 ))
-      (( level > 7 )) && level=7
-      blocks=("▁" "▂" "▃" "▄" "▅" "▆" "▇" "█") 
-      bars+="${blocks[$level]}"
+      level=$(( n / 100 ))  # More granular levels for smoother animation
+      (( level > 10 )) && level=10
+      
+      # Enhanced bar characters for smoother appearance
+      blocks=("⠀" "▁" "▂" "▃" "▄" "▅" "▆" "▇" "█" "🮂" "🮃")
+      bar="${blocks[$level]}"
+      
+      # Color gradient from blue -> cyan -> green -> yellow -> red
+      if (( level <= 2 )); then
+        color="#5e81ac"  # Blue
+      elif (( level <= 4 )); then
+        color="#88c0d0"  # Cyan  
+      elif (( level <= 6 )); then
+        color="#a3be8c"  # Green
+      elif (( level <= 8 )); then
+        color="#ebcb8b"  # Yellow
+      else
+        color="#bf616a"  # Red
+      fi
+      
+      bars+="<span color='$color'>$bar</span>"
     fi
   done
 
